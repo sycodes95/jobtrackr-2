c3e7e7736d472519c78c5afb7c429a538a6cf914
sycodes95 <sycodes95@gmail.com>
2024-01-19 00:30:00 -0800

    created a hook that returns pg User using sub from auth 0 user object

---
+++ src/app/hooks/useUserId.tsx
@@ -0,0 +1,26 @@
+
+'use client'
+
+import { useEffect, useState } from "react"
+import { UserType } from "../api/users/route"
+import { useUser } from "@auth0/nextjs-auth0/client";
+import { getUser } from "../services/getUser";
+
+export default function useUserId () {
+  const { user, error, isLoading } = useUser();
+  const [sub, setSub] = useState<string>('')
+  const [pgUser, setPGUser] = useState<UserType | null>(null)
+
+  useEffect(() => {
+    if(user && user.sub && !error && !isLoading) setSub(user.sub)
+  },[user, error, isLoading])
+
+  useEffect(()=> {
+    if(sub) {
+      getUser(sub).then(user => setPGUser(user))
+    }
+  },[sub])
+
+  if(pgUser) return { pgUser }
+  return null
+}
