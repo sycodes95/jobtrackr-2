ba1ae63796d23c026fdc11c60340a4fff8af6b2f
sycodes95 <sycodes95@gmail.com>
2024-02-08 21:44:45 -0800

    added generate

--- drizzle/schema.ts
+++ drizzle/schema.ts
@@ -41,25 +41,3 @@ export const applications = pgTable('applications', {
 
 });
 
-
-
-// declaring enum in database
-// export const popularityEnum = pgEnum('popularity', ['unknown', 'known', 'popular']);
-
-
-// export const countries = pgTable('countries', {
-  // id: serial('id').primaryKey(),
-  // name: varchar('name', { length: 256 }),
-// }, (countries) => {
-  // return {
-    // nameIndex: uniqueIndex('name_idx').on(countries.name),
-  // }
-// });
-// 
-// export const cities = pgTable('cities', {
-  // id: serial('id').primaryKey(),
-  // name: varchar('name', { length: 256 }),
-  // countryId: integer('country_id').references(() => countries.id),
-  // popularity: popularityEnum('popularity'),
-// });
-// 
