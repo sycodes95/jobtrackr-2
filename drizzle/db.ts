823d83e7a603ec085a8f74e1ee0763f7e6375658
sycodes95 <sycodes95@gmail.com>
2024-02-14 18:15:33 -0800

    used pool for db connection instead of client due to connection failures

--- drizzle/db.ts
+++ drizzle/db.ts
@@ -4,13 +4,23 @@ import { Client, Pool } from "pg";
 import * as schema from '../drizzle/schema'
 
 
-const client = new Client({
+// const client = new Client({
+//   host: process.env.PG_HOST,
+//   port: Number(process.env.PG_PORT),
+//   user: process.env.PG_USER,
+//   password: process.env.PG_PW,
+//   database: process.env.PG_DB_NAME,
+// });
+
+const pool = new Pool({
   host: process.env.PG_HOST,
   port: Number(process.env.PG_PORT),
   user: process.env.PG_USER,
   password: process.env.PG_PW,
   database: process.env.PG_DB_NAME,
-});
 
-await client.connect();
-export const db = drizzle(client, { schema });
+});
+export const db = drizzle(pool);
+
+// await client.connect();
+// export const db = drizzle(client, { schema });
