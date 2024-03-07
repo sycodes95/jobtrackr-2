fa3e0da12f1c2c8c6bcecbd7192e31d2ca48143f
sycodes95 <sycodes95@gmail.com>
2024-03-07 01:06:56 -0800

    implemented docker

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -7,6 +7,7 @@ export default {
   dbCredentials: {
     host: process.env.PG_HOST ?? '',
     user: process.env.PG_USER,
+    port: Number(process.env.PG_PORT),
     password: process.env.PG_PW,
     database: process.env.PG_DB_NAME ?? '',
   },
--- src/app/applications/components/appModal.tsx
+++ src/app/applications/components/appModal.tsx
@@ -1,31 +1,31 @@
-'use client'
+// 'use client'
 
-import {
-  Sheet,
-  SheetContent,
-  SheetDescription,
-  SheetHeader,
-  SheetTitle,
-  SheetTrigger,
-} from "@/components/ui/sheet"
+// import {
+//   Sheet,
+//   SheetContent,
+//   SheetDescription,
+//   SheetHeader,
+//   SheetTitle,
+//   SheetTrigger,
+// } from "@/components/ui/sheet"
 
-import ApplicationForm from "./applicationForm"
+// import ApplicationForm from "./applicationForm"
 
-interface AppModalProps {
-  button: React.ReactNode;
-}
+// interface AppModalProps {
+//   button: React.ReactNode;
+// }
 
-export default function AppModal ( { button } : AppModalProps ) {
-  return (
-    <Sheet>
-      <SheetTrigger className="w-fit flex justify-start">
-        {
-        button
-        }
-      </SheetTrigger>
-      <SheetContent side={'bottom'} className="min-h-[69%] max-w-7xl w-full border-2 border-border p-4 rounded-lg ">
-        <ApplicationForm />
-      </SheetContent>
-    </Sheet>
-  )
-}
+// export default function AppModal ( { button } : AppModalProps ) {
+//   return (
+//     <Sheet>
+//       <SheetTrigger className="w-fit flex justify-start">
+//         {
+//         button
+//         }
+//       </SheetTrigger>
+//       <SheetContent side={'bottom'} className="min-h-[69%] max-w-7xl w-full border-2 border-border p-4 rounded-lg ">
+//         <ApplicationForm />
+//       </SheetContent>
+//     </Sheet>
+//   )
+// }
